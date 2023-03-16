80976a0829d581cd4514cf7a8ef0701c438d9100
AmirElimelech <sapinhopreto@gmail.com>
2023-03-17 00:58:00 +0200

    made few changes in templates -not related to API

--- urls.py
+++ urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views.API_views import task_list_api, task_detail_api
 from .views.login_views import CustomLoginView, CustomLogoutView, SignUp
 from .views.task_views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from django.contrib.auth import views
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
-
+    
+   
 
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
@@ -18,4 +20,7 @@ urlpatterns = [
 
     path('api/', task_list_api, name='task-list-api'),
     path('api/<int:pk>/', task_detail_api, name='task-detail-api'),
+
+
+
 ]
